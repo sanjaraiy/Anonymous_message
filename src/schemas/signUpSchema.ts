03755afc4e09f2c76240abefbb9202841e8cb826
sanjaraiy <deepakrai78788@gmail.com>
2024-06-25 16:14:11 +0530

    project setup and schema validation with ZOD package

---
+++ src/schemas/signUpSchema.ts
@@ -0,0 +1,12 @@
+import {z} from 'zod'
+export const usernameValidation = z
+   .string()
+   .min(2, "Username must be atleast 2 characters")
+   .max(20, "Username must be no more than 20 characters")
+   .regex(/^[a-zA-Z][a-zA-Z0-9_-]{2,15}$/, "Username must not contain special character")
+
+export const signUpSchema = z.object({
+    username: usernameValidation,
+    email: z.string().email({message:"Invalid email address"}),
+    password: z.string().min(6, {message: "password must be at least 6 characters"})
+})
