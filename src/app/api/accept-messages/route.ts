be1873b242d29d3239dcacbf9a4c322f95cc898b
sanjaraiy <deepakrai78788@gmail.com>
2024-08-18 20:05:56 +0530

    removed some error at accepting-message route

--- src/app/api/accept-messages/route.ts
+++ src/app/api/accept-messages/route.ts
@@ -7,18 +7,15 @@ import {User} from 'next-auth';
 export async function POST(request: Request){
     await dbConnect();
 
-  const session = getServerSession(authOptions);
-  const user: User = session?.user;
+    const session = await getServerSession(authOptions);
+    const user: User = session?.user as User;
 
-  if(!session || !session.user){
-     return Response.json(
-        {
+    if(!session || !session.user){
+        return Response.json({
             success: false,
-            message: "Not Authenticated"
-        },
-        {status: 401}
-     )
-  }
+            message:'Not Authenticated'
+        }, {status: 401})
+    }
 
   const userId = user._id;
   const {acceptMessages} = await request.json();
